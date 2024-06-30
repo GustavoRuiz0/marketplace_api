dc4a7ff99605ebac2c0945ad3a93b17cd2e6e41d
gustavoruiz <gustavolvruiz8@gmail.com>
2024-06-30 16:32:20 -0300

    finish

---
+++ lib/api_constraints.rb
@@ -0,0 +1,12 @@
+class ApiConstraints
+  attr_reader :options
+
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(request)
+    @default || request.headers['Accept'].include?("application/localhost:3000.v#{@version}")
+  end
+end
