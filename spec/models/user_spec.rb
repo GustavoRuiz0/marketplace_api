4ff88cc1c4ae4efd377f81f0f1dc392efbf34d83
gustavoruiz <gustavolvruiz8@gmail.com>
2024-07-03 23:09:43 -0300

    test with shouldar

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe User, type: :model do
   it {should respond_to(:email)}
   it {should respond_to(:password)}
 
-  it {should be_valid}
+  describe 'When email is present' do 
+    it {should be_valid}
+  end
+
+  describe 'When email is not present' do 
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity}
+    it { should validate_presence_of(:email)}
+    it { should allow_value('email@domain.com').for(:email) }
+  end
 end
